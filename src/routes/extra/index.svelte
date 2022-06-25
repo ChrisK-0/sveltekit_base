3e3f1af7d49babfd6cb45c1d31c5ece9a2869226
ChrisK-0 <chris.kottisse@hotmail.com>
2022-06-25 16:50:45 +0300

    first commit

---
+++ src/routes/extra/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+	import { onMount } from 'svelte';
+
+	if (browser) {
+		onMount(() => {});
+	}
+</script>
+
+<svelte:head>
+	<title>Extra</title>
+	<meta name="description" />
+</svelte:head>
+
+<div>hey</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
